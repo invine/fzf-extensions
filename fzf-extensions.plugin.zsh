731bc13239652c9007644a91744283d262ffa9c6
Pavel Frolov <artyhere@gmail.com>
2019-09-23 20:51:58 +0300

    support arch native fzf installation

--- fzf-extensions.plugin.zsh
+++ fzf-extensions.plugin.zsh
@@ -1,5 +1,7 @@
 # Fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
+[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
 
 export FZF_DEFAULT_COMMAND='fd --type f --color=never'
 export FZF_ALT_C_COMMAND='fd --type d . --color=never'
